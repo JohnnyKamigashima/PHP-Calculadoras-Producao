b6cb152af5df7de13927b93b1a80a1bb4c72318c
Johnny Kamigashima <johnny@jhk.app>
2023-04-08 12:48:25 -0300

    Mudou caminho

--- partials/functions/test/EscalaPlano.test.js
+++ partials/functions/test/EscalaPlano.test.js
@@ -1,10 +1,11 @@
-const escalaPlano = require("../EscalaPlano_lib");
+const escalaPlano = require("functions/EscalaPlano_lib.js")
+
 describe('Testa a escala do Plano dado tamanho atual e cota', () => {
     it.each([
         [110, 50, "45.45"],
         [-15, 50, "-333.33"],
         [45.667, 1000, "2189.77"]
     ])("Escala do Plano: %i", (medidaAtual, medidaReferencia, resultado) => {
-        expect(escalaPlano(medidaAtual, medidaReferencia)).toBe(resultado);
+        expect(escalaPlano(medidaAtual, medidaReferencia)).toBe(resultado)
     })
-});
+})
